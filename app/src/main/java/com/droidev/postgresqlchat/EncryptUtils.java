77fc91b3764c62bb246fd756cb9f5536ebd6dd3d
none <none@>
2024-03-13 19:12:26 -0300

    change to encryption

--- app/src/main/java/com/droidev/postgresqlchat/EncryptUtils.java
+++ app/src/main/java/com/droidev/postgresqlchat/EncryptUtils.java
@@ -1,45 +1,43 @@
 package com.droidev.postgresqlchat;
 
-
-import android.content.Context;
-
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
-
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+
 public class EncryptUtils {
     private static final String ALGORITHM = "AES";
 
-    public static String encryptString(String stringToEncrypt, String key) throws Exception {
-
-        SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), ALGORITHM);
-        Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        return Base64.getEncoder().encodeToString(cipher.doFinal(stringToEncrypt.getBytes()));
-    }
-
-    public String generateSecretKey() throws NoSuchAlgorithmException {
-
+    public static String generateKey() throws Exception {
         KeyGenerator keyGenerator = KeyGenerator.getInstance(ALGORITHM);
-        keyGenerator.init(256); // AES key size
+        keyGenerator.init(256);
         SecretKey secretKey = keyGenerator.generateKey();
-
         return Base64.getEncoder().encodeToString(secretKey.getEncoded());
-
     }
 
-    public static String decryptString(String stringToDecrypt, String secretKey) throws Exception {
-        SecretKey key = new SecretKeySpec(Base64.getDecoder().decode(secretKey), ALGORITHM);
+    public static String encrypt(String data, Key key) throws Exception {
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedBytes = cipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    public static String decrypt(String encryptedData, String key) throws Exception {
+
+        SecretKey secretKey = convertStringToKey(key);
 
         Cipher cipher = Cipher.getInstance(ALGORITHM);
-        cipher.init(Cipher.DECRYPT_MODE, key);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedData));
+        return new String(decryptedBytes, StandardCharsets.UTF_8);
+    }
 
-        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(stringToDecrypt));
-
-        return new String(decryptedBytes);
+    public static SecretKey convertStringToKey(String keyString) {
+        byte[] keyBytes = Base64.getDecoder().decode(keyString);
+        return new SecretKeySpec(keyBytes, ALGORITHM);
     }
 }
