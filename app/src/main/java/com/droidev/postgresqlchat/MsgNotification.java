da7d60b910e93bb48e35c2f1a3bbdc96bc796062
none <none@>
2024-04-20 15:46:33 -0300

    implementing Alarm Manager to show notifications instead.

--- app/src/main/java/com/droidev/postgresqlchat/MsgNotification.java
+++ app/src/main/java/com/droidev/postgresqlchat/MsgNotification.java
@@ -2,7 +2,9 @@ package com.droidev.postgresqlchat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 
 import androidx.core.app.NotificationCompat;
 
@@ -17,10 +19,15 @@ public class MsgNotification {
         );
         notificationManager.createNotificationChannel(channel);
 
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_MUTABLE);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "psqlchat_channel")
                 .setContentTitle(title)
                 .setContentText(msgs)
                 .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
 
         NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
